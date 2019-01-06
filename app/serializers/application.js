d20b5fa143bd26ad37c1a812887fef1a8865f28a
Aad Versteden <madnificent@gmail.com>
2019-01-06 18:41:23 +0100

    Generate webuniversum styling
    This styling ensures the pages are rendered in the style of the
    Flemish Government.

--- app/serializers/application.js
+++ app/serializers/application.js
@@ -2,4 +2,5 @@ import DS from 'ember-data';
 import DataTableSerializerMixin from 'ember-data-table/mixins/serializer';
 
 export default DS.JSONAPISerializer.extend(DataTableSerializerMixin, {
+
 });
