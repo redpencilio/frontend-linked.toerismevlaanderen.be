48d5dc14d4ab853a852d09661b814e41f5ebad81
Aad Versteden <madnificent@gmail.com>
2019-01-07 12:04:28 +0100

    Generating controllers for data-table pages
    These controllers should help in removing the NaN display in the
    rendered pages.

---
+++ app/controllers/rooms/index.js
@@ -0,0 +1,6 @@
+import Controller from '@ember/controller';
+import Ember from 'ember';
+import DefaultQueryParamsMixin from 'ember-data-table/mixins/default-query-params';
+
+export default Controller.extend(DefaultQueryParamsMixin, {
+});
