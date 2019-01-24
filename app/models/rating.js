1d0b9c4877fd7dc88135b94dd5b686b5f05caa18
Erika Pauwels <erika.pauwels@gmail.com>
2019-01-24 15:43:06 +0100

    Update string representation of rating

--- app/models/rating.js
+++ app/models/rating.js
@@ -1,12 +1,17 @@
 import attr from 'ember-data/attr';
 import Model from 'ember-data/model';
-import { collect } from '@ember/object/computed';
+import { computed } from '@ember/object';
 import { belongsTo } from 'ember-data/relationships';
 
 export default Model.extend({
   // A string representation of this model, based on its attributes.
   // This is what mu-cl-resources uses to search on, and how the model will be presented while editing relationships.
-  stringRep: collect.apply(this,['id', 'ratingValue', 'bestRating', 'worstRating']),
+  stringRep: computed('ratingValue', 'bestRating', 'worstRating', function() {
+    let str = this.ratingValue;
+    if (this.bestRating && this.worstRating)
+      str += ` [${this.worstRating}-${this.bestRating}]`;
+    return str;
+  }),
 
   uri: attr(),
   ratingValue: attr(),
