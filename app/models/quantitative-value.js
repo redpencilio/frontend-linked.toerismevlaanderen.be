177dc3968b4a28c8953ce1f1a187db0d6589d27e
Erika Pauwels <erika.pauwels@gmail.com>
2019-02-07 10:04:12 +0100

    Update string representation of quantitative value

--- app/models/quantitative-value.js
+++ app/models/quantitative-value.js
@@ -1,12 +1,15 @@
 import attr from 'ember-data/attr';
 import Model from 'ember-data/model';
-import { collect } from '@ember/object/computed';
+import { computed } from '@ember/object';
 import { belongsTo } from 'ember-data/relationships';
 
 export default Model.extend({
   // A string representation of this model, based on its attributes.
   // This is what mu-cl-resources uses to search on, and how the model will be presented while editing relationships.
-  stringRep: collect.apply(this,['id', 'unit', 'value']),
+  stringRep: computed('unit', 'value', function() {
+    return `${this.unit}: ${this.value}`;
+  }),
+
 
   uri: attr(),
   unit: attr('language-string-set'),
