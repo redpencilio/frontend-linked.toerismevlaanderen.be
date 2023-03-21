f80681ec97c0bcd045614a90a54983dad65fbb1c
Erika Pauwels <erika.pauwels@gmail.com>
2023-03-21 14:46:35 +0100

    Show latest dataset per type/extension on download page

--- app/models/concept.js
+++ app/models/concept.js
@@ -1,6 +1,7 @@
 import Model, { attr, hasMany } from '@ember-data/model';
 
 export default class ConceptModel extends Model {
+  @attr uri;
   @attr label;
 
   @hasMany('dataset', { inverse: 'type', async: true }) datasets;
--- app/routes/datasets.js
+++ app/routes/datasets.js
@@ -5,11 +5,29 @@ export default class DatasetsRoute extends Route {
   @service store;
 
   async model() {
-    const datasets = await this.store.query('dataset', {
-      page: { size: 1 },
-      sort: '-modified',
-      include: 'distributions',
+    const datasetTypes = await this.store.query('concept', {
+      sort: 'label',
+      'filter[concept-schemes][:uri:]': 'http://themis.vlaanderen.be/id/concept-scheme/a76297cd-ba80-468c-bc18-4b9e1117eee9'
     });
-    return datasets[0];
+
+    // Get the latest version for each dataset type
+    const datasets = (await Promise.all(
+      datasetTypes.toArray().map(async (datasetType) => {
+        return (await Promise.all(
+          ['ttl', 'csv'].map(async (extension) => {
+            const datasets = await this.store.query('dataset', {
+              page: { size: 1 },
+              sort: '-modified',
+              include: 'distributions',
+              'filter[type][:uri:]': datasetType.uri,
+              'filter[distributions][extension]': extension
+            });
+            return datasets[0];
+          })
+        )).flat();
+      })
+    )).flat();
+
+    return datasets.filter((d) => d).sortBy('modified').reverse();
   }
 }
