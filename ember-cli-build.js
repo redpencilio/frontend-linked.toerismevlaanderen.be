2ba2430ed0c92270f13119155d3a8ed94b2a1ac8
Erika Pauwels <erika.pauwels@gmail.com>
2024-01-22 16:56:40 +0100

    Bump ember-appuniversum

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,7 +4,12 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function (defaults) {
   const app = new EmberApp(defaults, {
-    // Add options here
+    sassOptions: {
+      includePaths: ["node_modules/@appuniversum/ember-appuniversum"],
+    },
+    '@appuniversum/ember-appuniversum': {
+      disableWormholeElement: true,
+    },
   });
 
   // Use `app.import` to add additional libraries to the generated
