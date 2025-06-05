2cd2511c4715333af6651dfa6d782374c33f7bb8
Erika Pauwels <erika.pauwels@gmail.com>
2025-06-05 11:46:53 +0200

    Bump ember-metis

--- app/router.js
+++ app/router.js
@@ -1,6 +1,6 @@
 import EmberRouter from '@ember/routing/router';
 import config from 'frontend-tvl-subject-pages/config/environment';
-import metisFallbackRoute from 'metis/utils/fallback-route';
+import { fallbackRoute, externalRoute } from 'ember-metis';
 
 export default class Router extends EmberRouter {
   location = config.locationType;
@@ -14,5 +14,6 @@ Router.map(function () {
   this.route('attractions');
   this.route('examples');
 
-  metisFallbackRoute(this);
+  externalRoute(this);
+  fallbackRoute(this);
 });
